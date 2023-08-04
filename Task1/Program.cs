cf2f48df92200fc27b6524611ff4f7749e0beefb
Daniil <daniilkukotsky@gmail.com>
2023-08-04 19:15:45 +0400

    Final commit Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿int ShowNums(int usernum, int finishnum)
+{
+    if (usernum < 1) 
+    {
+        return finishnum;
+    }
+    Console.Write(usernum + " ");
+    usernum--;
+    return ShowNums(usernum, finishnum);
+}
+
+Console.Write("How many numbers to print: ");
+int usernumber = Convert.ToInt32(Console.ReadLine());
+ShowNums(usernumber, 1);
